aaf1781494b1d6dcd732e767bc76dc301b817f4b
PepperGo <meetmark.sun@gmail.com>
2017-12-20 17:38:06 -0600

    todoProject Step8 Adding user account

--- Framework/Meteor/Project/simple-todos/imports/ui/body.js
+++ Framework/Meteor/Project/simple-todos/imports/ui/body.js
@@ -1,3 +1,4 @@
+import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import { ReactiveDict } from 'meteor/reactive-dict';
 
@@ -21,7 +22,7 @@ Template.body.helpers({
     // Otherwise, return all of the tasks
       return Tasks.find({}, { sort: { createdAt: -1 } });
   },
-   incompleteCount() {
+  incompleteCount() {
     return Tasks.find({ checked: { $ne: true } }).count();
   },
 });
@@ -39,6 +40,8 @@ Template.body.events({
     Tasks.insert({
       text,
       createdAt: new Date(), // current time
+	  owner: Meteor.userId(),
+      username: Meteor.user().username,
     });
  
     // Clear form
