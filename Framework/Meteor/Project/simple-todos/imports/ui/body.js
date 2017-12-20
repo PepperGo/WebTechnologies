74a65628e91a79e764c10b9d85267ee90fed9f94
PepperGo <meetmark.sun@gmail.com>
2017-12-20 17:29:41 -0600

    todoProject Step7

--- Framework/Meteor/Project/simple-todos/imports/ui/body.js
+++ Framework/Meteor/Project/simple-todos/imports/ui/body.js
@@ -1,14 +1,28 @@
 import { Template } from 'meteor/templating';
- 
+import { ReactiveDict } from 'meteor/reactive-dict';
+
 import { Tasks } from '../api/tasks.js';
 
 import './task.js'; 
 
 import './body.html';
  
+Template.body.onCreated(function bodyOnCreated() {
+  this.state = new ReactiveDict();
+});
+ 
 Template.body.helpers({
   tasks() {
+	const instance = Template.instance();
+    if (instance.state.get('hideCompleted')) {
+      // If hide completed is checked, filter tasks
+      return Tasks.find({ checked: { $ne: true } }, { sort: { createdAt: -1 } });
+    }
+    // Otherwise, return all of the tasks
       return Tasks.find({}, { sort: { createdAt: -1 } });
+  },
+   incompleteCount() {
+    return Tasks.find({ checked: { $ne: true } }).count();
   },
 });
 
@@ -30,4 +44,9 @@ Template.body.events({
     // Clear form
     target.text.value = '';
   },
+  
+  'change .hide-completed input'(event, instance) {
+    instance.state.set('hideCompleted', event.target.checked);
+  },
+  
 });
