f59e455c6ab3d9bd8874b0fdad0049187b62b23b
PepperGo <meetmark.sun@gmail.com>
2017-12-20 14:39:46 -0600

    todoProject mongo collection

--- Framework/Meteor/Project/simple-todos/imports/components/todosList/todosList.js
+++ Framework/Meteor/Project/simple-todos/imports/components/todosList/todosList.js
@@ -1,23 +1,23 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
+import { Tasks } from '../../api/tasks.js';
+ 
 import template from './todosList.html';
  
 class TodosListCtrl {
-  constructor() {
-    this.tasks = [{
-      text: 'This is task 1'
-    }, {
-      text: 'This is task 2'
-    }, {
-      text: 'This is task 3'
-    }];
+  constructor($scope) {
+    $scope.viewModel(this);
+ 
+    this.helpers({
+      tasks() {
+        return Tasks.find({});
+      }
+    })
   }
 }
- 
-export default angular.module('todosList', [
-  angularMeteor
-])
+
+export default angular.module('todosList', [])
   .component('todosList', {
     templateUrl: 'imports/components/todosList/todosList.html',
-    controller: TodosListCtrl
+    controller: ['$scope', TodosListCtrl]
   });
